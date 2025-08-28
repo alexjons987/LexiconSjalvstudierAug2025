1ba497c1fa85f0f99c933075e7c34de240b090ba
alexjons987 <alexander.jonsson987@outlook.com>
2025-08-28 15:23:33 +0200

    Add Switch section

--- src/Main.java
+++ src/Main.java
@@ -271,6 +271,38 @@ public class Main {
 
         String xGreaterThanYString = x > y? "x is greater than y" : "x is not greater than y";
         System.out.println(xGreaterThanYString);
+
+        //
+        // Java - Switch
+        //
+        System.out.println("\nJava - Switch");
+        int dayOfTest = 2;
+        switch (dayOfTest) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("No day beyond 7 or before 1!");
+                break;
+        }
     }
 
     public static void getStarted() {
